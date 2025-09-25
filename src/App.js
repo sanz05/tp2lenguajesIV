a92d918293a196261042740773ddf623ee5aa974
sanz05 <facundo.sanz05@gmail.com>
2025-09-25 17:00:23 -0300

    SUBO tp3
    Subo Archivos Tp2y3

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./paginas/Home";
 import Contacto from "./paginas/Contacto";
 
-
 function App() {
   return (
-    
     <Router>
       <nav style={{ padding: "10px", background: "#eee" }}>
         <Link to="/" style={{ marginRight: "10px" }}>Inicio</Link>
--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ImageUploader from "../componentes/CargadorImagen";
+import SplitText from "../componentes/SplitText";
+
 
 function Home() {
   return (
     <div>
-      <h1>Página Principal</h1>
+      
+      <div style={{ textAlign: "center", padding: "2rem" }}>
+      <SplitText
+        text="Bienvenido al Cargador de Imágenes"
+        splitType="chars"   // o "words"
+        delay={50}
+        duration={0.6}
+        tag="h1"
+      />
+    </div>
       <ImageUploader />
     </div>
   );
